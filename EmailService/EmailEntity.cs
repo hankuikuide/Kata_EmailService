125e526ec778e174d9e3e231524bdeae288464e5
hankuikuide <hankuikuide@163.com>
2013-12-26 17:11:45 +0800

    create
    created by Tomson

---
+++ EmailService/EmailEntity.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EmailService
+{
+    class EmailEntity
+    {
+        private string emailId;
+
+        public string EmailId
+        {
+            get { return emailId; }
+            set { emailId = value; }
+        }
+
+        private string from;
+        //发件人
+        public string From
+        {
+            get { return from; }
+            set { from = value; }
+        }
+
+        private string to;
+        //收件人
+        public string To
+        {
+            get { return to; }
+            set { to = value; }
+        }
+
+        private string cc;
+        //抄送
+        public string Cc
+        {
+            get { return cc; }
+            set { cc = value; }
+        }
+
+        private string bcc;
+        //密送
+        public string Bcc
+        {
+            get { return bcc; }
+            set { bcc = value; }
+        }
+
+        private DateTime date;
+        //日期
+        public DateTime Date
+        {
+            get { return date; }
+            set { date = value; }
+        }
+
+        private string subject;
+        //主题
+        public string Subject
+        {
+            get { return subject; }
+            set { subject = value; }
+        }
+
+        private string content;
+        //正文内容
+        public string Content
+        {
+            get { return content; }
+            set { content = value; }
+        }
+
+
+        private List<string> filesize = new List<string>();
+        //附件大小
+        public List<string> Filesize
+        {
+            get { return filesize; }
+            set { filesize = value; }
+        }
+
+        private string attachment = string.Empty;
+        //附件名称
+        public string Attachment
+        {
+            get { return attachment; }
+            set { attachment = value; }
+        }
+        private string attachmentUrl = string.Empty;
+        //附件链接
+        public string AttachmentUrl
+        {
+            get { return attachmentUrl; }
+            set { attachmentUrl = value; }
+        }
+    }
+}
---
+++ EmailService/UserEntity.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EmailService
+{
+    public class UserEntity
+    {
+        private string emailAddress;
+
+        public string EmailAddress
+        {
+            get { return emailAddress; }
+            set { emailAddress = value; }
+        }
+        private string pop3Address;
+
+        public string Pop3Address
+        {
+            get { return pop3Address; }
+            set { pop3Address = value; }
+        }
+        private string pop3Port;
+
+        public string Pop3Port
+        {
+            get { return pop3Port; }
+            set { pop3Port = value; }
+        }
+        private string smtpAddress;
+
+        public string SmtpAddress
+        {
+            get { return smtpAddress; }
+            set { smtpAddress = value; }
+        }
+        private string smtpPort;
+
+        public string SmtpPort
+        {
+            get { return smtpPort; }
+            set { smtpPort = value; }
+        }
+        private string passWord;
+
+        public string PassWord
+        {
+            get { return passWord; }
+            set { passWord = value; }
+        }
+    }
+}
